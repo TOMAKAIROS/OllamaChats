d56dce06c92e6c3ab244423debb2d492eb959eec
tomakairos <daedalus.vang@gmail.com>
2025-11-23 17:10:35 -0600

    converse.js updated.

--- js/converse.js
+++ js/converse.js
@@ -1,5 +1,23 @@
 document.getElementById("start_btn").addEventListener("click", () => {
+    const apiUrl = document.getElementById("api_url").value; 
+    const model = document.getElementById("model").value;
+    const prompt = document.getElementById("prompt").value;
 
-console.log("testing output");
-
+    chrome.runtime.sendMessage(
+        {
+            type: "converse",
+            baseUrl: apiUrl,
+            model: model,
+            prompt: prompt
+        },
+        (response) => {
+            if (!response || !response.success) {
+                console.log(response);
+                alert("OllamaChats failed to converse, please try again.");
+                return;
+            }
+            // console.log("OllamaChats conversed successfully!");
+            // console.log(response);
+        }
+    );
 });
